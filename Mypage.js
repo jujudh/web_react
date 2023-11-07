5c634213628159a4c5d0ce7e4e16c22740bd4d2d
jujudh <88125527+jujudh@users.noreply.github.com>
2023-11-07 22:06:05 +0900

    Update Mypage.js

--- Mypage.js
+++ Mypage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import Contact from "./Contact";
+import SelectOption from "./SelectOption";
 
 const Mypage = (props) => {
     const history = useHistory();
@@ -13,7 +14,9 @@ const Mypage = (props) => {
         <>
             <h1>My Page</h1>
             <br />
-            <button onClick={goBack}>Go Back</button>
+            <button onClick={goBack}>시작하기</button>
+
+            <SelectOption /> {/* SelectOption 컴포넌트를 렌더링 */}
         </>
     );
 };
