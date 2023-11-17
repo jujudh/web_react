8263bf83ecca8e7856810437a52b76915033ed8c
jujudh <88125527+jujudh@users.noreply.github.com>
2023-11-17 11:09:30 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home'; // Home 컴포넌트 파일 이름에 맞게 변경하세요.
 import Contact from './Contact';
-import Mypage from "./Mypage"; // Contact 컴포넌트 파일 이름에 맞게 변경하세요.
-
+import Mypage from "./Mypage";
+import Download from "./Download";
+import axios from "axios";
+import StopQuiz from "./StopQuiz";
 const App = () => {
     return (
         <Router>
@@ -11,6 +13,8 @@ const App = () => {
                 <Route path="/" exact component={Home} />
                 <Route path="/Contact" component={Contact} />
                 <Route path="/Mypage" component={Mypage} />
+                <Route path="/Download" component={Download} />
+                <Route path="/stop" component={StopQuiz} />
             </Switch>
         </Router>
     );
