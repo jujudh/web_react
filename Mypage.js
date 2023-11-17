d72594ab3b541f1bb505c691be23d12792f6233a
jujudh <88125527+jujudh@users.noreply.github.com>
2023-11-17 15:54:12 +0900

    Update Mypage.js

--- Mypage.js
+++ Mypage.js
@@ -1,23 +1,60 @@
-import React from "react";
-import { useHistory } from "react-router-dom";
-import Contact from "./Contact";
-import SelectOption from "./SelectOption";
+import React, { useState } from "react";
+import { useHistory, Link } from "react-router-dom";
+import ExamOptionList from "./ExamOptionList";
+import CheckboxGroup from "./CheckboxGroup";
 
-const Mypage = (props) => {
+const Mypage = () => {
     const history = useHistory();
+
+    const [examName, setExamType] = useState({
+        dropdown1:'국가기술자격시험',
+        dropdown2:'기능장',
+        examType: '가스기능장',
+        // 다른 초기값들도 필요에 따라 설정
+    });
+
+    const [examOptions, setExamOptions] = useState({
+        year: 2022,
+        version: 1,
+        answerType: '교사용'
+        // 다른 초기값들도 필요에 따라 설정
+    });
 
     const goBack = () => {
         history.goBack();
     };
 
     return (
-        <>
-            <h1>My Page</h1>
+        <div className='Mypage'>
+            <p>
+                <h1>My Page</h1>
+                <button onClick={goBack}>뒤로가기</button>
+            </p>
             <br />
-            <button onClick={goBack}>시작하기</button>
+            <br />
+            <ExamOptionList onChange={(newOptions) => setExamType(newOptions)} />
+            <br />
+            <CheckboxGroup onChange={(newOptions) => setExamOptions(newOptions)} />
+            {/* setExamType로 수정 */}
+            <br />
+            <Link
+                to={{
+                    pathname: '/download',
+                    state: {
+                        examType: { ...examName }, // examType을 전달
+                        options: { ...examOptions }
+                    },
+                }}
+            >
+                <button>다운받기</button>
+            </Link>
+            <br />
 
-            <SelectOption /> {/* SelectOption 컴포넌트를 렌더링 */}
-        </>
+            <Link to="/quizgo">
+                <button>문제 풀기</button>
+            </Link>
+
+        </div>
     );
 };
 export default Mypage;
